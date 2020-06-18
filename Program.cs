dd8a183bcba567efe498dbd06fe2c3a12b3cd7aa
NicollahSekete <nicollahtsekete@gmail.com>
2020-06-18 16:19:16 +0100

    4th commit

--- Program.cs
+++ Program.cs
@@ -20,30 +20,16 @@ namespace piechallengecshap
     {
         static void Main(string[] args)
         {
-           //PieTime pieUp = new PieTime();
-           //int modMe = pieUp.Pie(1,4);
-          // Console.WriteLine(modMe);
-           {  
-          int n, i, m=0, flag=0;    
-          Console.Write("Enter the Number to check Prime: ");    
-          n = int.Parse(Console.ReadLine());  
-          m=n/2;    
-          for(i = 2; i <= m; i++)    
-          {    
-           if(n % i == 0)    
-            {    
-             Console.Write("Number is not Prime.");    
-             flag=1;    
-             break;    
-            }    
-          }    
-          if (flag==0)    
-           Console.Write("Number is Prime.");       
-     }  
-       
 
-          
+               Prime findPrime = new Prime();
+               findPrime.primeNumber();
+
+               Palindome findPalindome = new Palindome();
+               findPalindome.palin();
+        }
+           
+            
             
         }
     }
-}
+
--- palindrome.cs
+++ palindrome.cs
@@ -1,8 +1,24 @@
+using System;
+
 namespace piechallengecshap{
 
     class Palindome{
         public void palin()
         {
+            int a,r,sum=0,temp;    
+          Console.Write("Enter the Number: ");   
+          a = int.Parse(Console.ReadLine());  
+          temp=a;      
+          while(a>0)      
+          {      
+           r=a%10;      
+           sum=(sum*10)+r;      
+           a=a/10;      
+          }      
+          if(temp==sum)      
+           Console.Write("Number is Palindrome.");      
+          else      
+           Console.Write("Number is not Palindrome");   
 
         }
     
